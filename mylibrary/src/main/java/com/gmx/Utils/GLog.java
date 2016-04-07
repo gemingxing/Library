5cff29e8e8574ab5f4d5d85423f06b23f2ce8316
MingXing <929239522@qq.com>
2016-04-07 12:37:54 +0800

    添加打印Log工具(2016.4.7) 修改KLog=>GLog

--- mylibrary/src/main/java/com/gmx/Utils/GLog.java
+++ mylibrary/src/main/java/com/gmx/Utils/GLog.java
@@ -1,13 +1,16 @@
-package com.gmx.log;
+package com.gmx.Utils;
 
 import java.io.File;
 
 /**
  * Created by MingXing on 16/4/7.
+ *
+ * Log工具
+ *
  * <p/>
  * implements https://github.com/ZhaoKaiQiang/KLog
  */
-public class GLog implements IGLog {
+public class GLog {
 
     /**
      * 初始化
@@ -18,124 +21,99 @@ public class GLog implements IGLog {
         com.socks.library.KLog.init(isShowLog);
     }
 
-
-    @Override
-    public void v(Object msg) {
+    public static void v(Object msg) {
         com.socks.library.KLog.v(msg);
     }
 
-    @Override
-    public void v(String tag, Object... objects) {
+    public static void v(String tag, Object... objects) {
         com.socks.library.KLog.v(tag, objects);
     }
 
-    @Override
-    public void d() {
+    public static void d() {
         com.socks.library.KLog.d();
     }
 
-    @Override
-    public void d(Object msg) {
+    public static void d(Object msg) {
         com.socks.library.KLog.d(msg);
     }
 
-    @Override
-    public void d(String tag, Object... objects) {
+    public static void d(String tag, Object... objects) {
         com.socks.library.KLog.d(tag, objects);
     }
 
-    @Override
-    public void i() {
+    public static void i() {
         com.socks.library.KLog.i();
     }
 
-    @Override
-    public void i(Object msg) {
+    public static void i(Object msg) {
         com.socks.library.KLog.i(msg);
     }
 
-    @Override
-    public void i(String tag, Object... objects) {
+    public static void i(String tag, Object... objects) {
         com.socks.library.KLog.i(tag, objects);
     }
 
-    @Override
-    public void w() {
+    public static void w() {
         com.socks.library.KLog.w();
     }
 
-    @Override
     public void w(Object msg) {
         com.socks.library.KLog.w(msg);
     }
 
-    @Override
-    public void w(String tag, Object... objects) {
+    public static void w(String tag, Object... objects) {
         com.socks.library.KLog.w(tag, objects);
     }
 
-    @Override
     public void e() {
         com.socks.library.KLog.e();
     }
 
-    @Override
-    public void e(Object msg) {
+    public static void e(Object msg) {
         com.socks.library.KLog.e(msg);
     }
 
-    @Override
-    public void e(String tag, Object... objects) {
+    public static void e(String tag, Object... objects) {
         com.socks.library.KLog.e(tag, objects);
     }
 
-    @Override
-    public void a() {
+    public static void a() {
         com.socks.library.KLog.a();
     }
 
-    @Override
-    public void a(Object msg) {
+    public static void a(Object msg) {
         com.socks.library.KLog.a(msg);
     }
 
-    @Override
-    public void a(String tag, Object... objects) {
+    public static void a(String tag, Object... objects) {
         com.socks.library.KLog.a(tag, objects);
     }
 
-    @Override
-    public void json(String jsonFormat) {
+    public static void json(String jsonFormat) {
         com.socks.library.KLog.json(jsonFormat);
     }
 
-    @Override
-    public void json(String tag, String jsonFormat) {
+    public static void json(String tag, String jsonFormat) {
         com.socks.library.KLog.json(tag, jsonFormat);
     }
 
-    @Override
-    public void xml(String xml) {
+    public static void xml(String xml) {
         com.socks.library.KLog.xml(xml);
     }
 
-    @Override
-    public void xml(String tag, String xml) {
+    public static void xml(String tag, String xml) {
         com.socks.library.KLog.xml(tag, xml);
     }
 
-    @Override
-    public void file(File targetDirectory, Object msg) {
+    public static void file(File targetDirectory, Object msg) {
         com.socks.library.KLog.file(targetDirectory, msg);
     }
 
-    @Override
-    public void file(String tag, File targetDirectory, Object msg) {
+    public static void file(String tag, File targetDirectory, Object msg) {
         com.socks.library.KLog.file(tag, targetDirectory, msg);
     }
 
-    @Override
-    public void file(String tag, File targetDirectory, String fileName, Object msg) {
+    public static void file(String tag, File targetDirectory, String fileName, Object msg) {
         com.socks.library.KLog.file(tag, targetDirectory, fileName, msg);
     }
 }
